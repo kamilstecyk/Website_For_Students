7f0e83918cd1882b3bb01468d6c721a6645ac42e
m-gibas <gibasmaksymilian@gmail.com>
2022-07-31 20:35:01 +0200

    studentsBook update

--- Website/js/booksScroll.js
+++ Website/js/booksScroll.js
@@ -9,6 +9,21 @@ function showBox(n) {
     let i;
     let books = document.getElementsByClassName("bookBar");
 
+
+    // something is no yes
+    let PLcheckBox = document.getElementById('plDep').checked;
+    let ENcheckBox = document.getElementById('enDep').checked;
+    if (PLcheckBox === true && ENcheckBox === false) {
+        books = document.getElementsByClassName("pl");
+    }
+    else if (ENcheckBox === true && PLcheckBox === false) {
+        books = document.getElementsByClassName("en");
+    }
+    else {
+        books = document.getElementsByClassName("bookBar");
+    }
+
+
     if(n > (books.length / 3) + 3) {
         boxIndex = 0;
     }
--- Website/js/topNavBar.js
+++ Website/js/topNavBar.js
@@ -1,11 +1,25 @@
+// function expand() {
+//     document.getElementById("topNavBarContent").style.display = "flex";
+
+//     document.getElementById("expandTopNavBar").onclick = collapse;
+// }
+
+// function collapse() {
+//     document.getElementById("topNavBarContent").style.display = "none";
+
+//     document.getElementById("expandTopNavBar").onclick = expand;
+// }
+
 function expand() {
-    document.getElementById("topNavBarContent").style.display = "flex";
+    document.getElementById("topNavBarContent").style.visibility = "visible";
+    document.getElementById("topNavBarContent").style.opacity = "1";
 
     document.getElementById("expandTopNavBar").onclick = collapse;
 }
 
 function collapse() {
-    document.getElementById("topNavBarContent").style.display = "none";
+    document.getElementById("topNavBarContent").style.visibility = "hidden";
+    document.getElementById("topNavBarContent").style.opacity = "0";
 
     document.getElementById("expandTopNavBar").onclick = expand;
 }
