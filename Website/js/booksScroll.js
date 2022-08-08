9c3f9c3154db71b0cf8976ea3707a17c903c1c9e
m-gibas <gibasmaksymilian@gmail.com>
2022-08-08 11:54:46 +0200

    books scroll update

--- Website/js/booksScroll.js
+++ Website/js/booksScroll.js
@@ -1,16 +1,50 @@
 let boxIndex = 0;
-showBox(3);
+showBox(1);
+
+document.getElementById('plDep').onclick = function() {
+    boxIndex = 0;
+    showBox(0);
+}
+
+document.getElementById('enDep').onclick = function() {
+    boxIndex = 0;
+    showBox(0);
+}
 
 function plusBox(n) {
     showBox(boxIndex += n);
 }
 
-function showBox(n) {
+function sliceArrayIntoGroups(arr, size) {
+    if (arr.length === 0) { return arr; }
+    return [ arr.slice(0, size), ...sliceArrayIntoGroups(arr.slice(size), size) ];
+}
 
-    function sliceArrayIntoGroups(arr, size) {
-        if (arr.length === 0) { return arr; }
-        return [ arr.slice(0, size), ...sliceArrayIntoGroups(arr.slice(size), size) ];
-      }
+
+// scroll function
+
+let scrollableElement = document.getElementById('mainLayoutStudentsBook');
+scrollableElement.addEventListener('wheel', findScrollDirectionOtherBrowsers);
+
+function findScrollDirectionOtherBrowsers(event){
+    let delta;
+    if (event.wheelDelta){
+        delta = event.wheelDelta;
+    }else{
+        delta = -1 *event.deltaY;
+    }
+
+    if (delta < 0){
+        plusBox(-1);
+    }else if (delta > 0){
+        plusBox(1);
+    }
+}
+
+// end of scroll function
+
+
+function showBox(n) {
 
 
     let checkBoxes = document.querySelectorAll('input[name="department"]:checked');
@@ -33,8 +67,8 @@ function showBox(n) {
     if (values.length == 1) {
         books = document.getElementsByClassName(values[0]);
         slicedBooks = sliceArrayIntoGroups(Array.from(books), 3);
-        console.log(books);
-        console.log(slicedBooks);
+        // console.log(books);
+        // console.log(slicedBooks);
         
         if(n > slicedBooks.length - 1) {
             boxIndex =  0;
